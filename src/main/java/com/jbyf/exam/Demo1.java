83e24fd6dd3d90a921ac9a92d05c17042ee4c529
jbyf <506921864@qq.com>
2021-06-08 19:50:26 +0800

    "第一次提交"

--- src/main/java/com/jbyf/exam/Demo1.java
+++ src/main/java/com/jbyf/exam/Demo1.java
@@ -3,5 +3,7 @@ package com.jbyf.exam;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello master");
+        System.out.println("Hello Demo2");
     }
 }
